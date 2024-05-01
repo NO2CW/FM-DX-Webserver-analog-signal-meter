ae3c747682f57e560981f5896de3e92575463fc0
NO2CW <149436256+NO2CW@users.noreply.github.com>
2024-05-01 13:14:41 -0400

    Add files via upload

---
+++ signal-meter-plugin.js
@@ -0,0 +1,14 @@
+// Plugin configuration, this is used in the administration when plugins are loaded
+var pluginConfig = {
+    name: 'Analog meter',
+    version: '1.0',
+    author: 'Ivan NO2CW',
+    frontEndPath: 'signal-meter-plugin/signal-meter-plugin.js'
+}
+
+// Backend (server) changes can go here...
+
+// Don't change anything below here if you are making your own plugin
+module.exports = {
+    pluginConfig
+}
